8fa9a63c09126c5c3e6f5dc8fa7c1460a034d81d
Levi Linville <levi.linville@inin.com>
2017-12-19 13:26:27 -0500

    Added 2d-state automata

---
+++ automata/Polar2DAutomata.py
@@ -0,0 +1,113 @@
+import random
+import utils
+import math
+from simplexnoise import scaled_octave_noise_3d
+import colorsys
+
+class Polar2DAutomata():
+    def __init__(self, numCells):
+        self.thetaSlots = 100
+        self.rSlots = 100
+        self.thetaSlotWidth = 1.0 / self.thetaSlots
+        self.rSlotWidth = 1.0 / self.rSlots
+
+        self.cells = [None for cell in range(numCells)]
+        self.numCells = len(self.cells)
+        self.transitionTable = []
+
+    def indexedTransition(self, thetaIndex, rIndex):
+        thetaIndex = int(thetaIndex % self.thetaSlots)
+        rIndex = int(rIndex % self.rSlots)
+        return self.transitionTable[thetaIndex][rIndex]
+
+    def transition(self, (x, y)):
+        theta = math.atan2(y, x) % (2 * math.pi)
+        theta /= 2 * math.pi
+        r = self.sigmoid(math.sqrt(x * x + y * y))
+
+        thetaIndex = math.floor(theta / self.thetaSlotWidth)
+        thetaInterpolateFraction = (theta % self.thetaSlotWidth) * self.thetaSlots
+        rIndex = math.floor(r / self.rSlotWidth)
+        rInterpolateFraction = (r % self.rSlotWidth) * self.rSlots
+
+        lowerThetaInterpolated = self.interpolate(
+            self.indexedTransition(thetaIndex, rIndex),
+            self.indexedTransition(thetaIndex, rIndex + 1),
+            rInterpolateFraction)
+
+        upperThetaInterpolated = self.interpolate(
+            self.indexedTransition(thetaIndex + 1, rIndex),
+            self.indexedTransition(thetaIndex + 1, rIndex + 1),
+            rInterpolateFraction)
+
+    #    return self.interpolate(lowerThetaInterpolated, upperThetaInterpolated, thetaInterpolateFraction)
+        return self.indexedTransition(thetaIndex, rIndex)
+
+
+    def interpolate(self, bound1, bound2, location):
+
+        return bound1[0] + location * (bound2[0] - bound1[0]), bound1[1] + location * (bound2[1] - bound1[1])
+
+
+    def step(self):
+        newCells = [None for cell in self.cells]
+        for cellIndex, cell in enumerate(self.cells):
+            newCells[cellIndex] = self.transition(cell)
+
+        self.cells = newCells
+
+    def sigmoid(self, x):
+        return 1 / (1 + math.exp(-x))
+
+    def randomizeRules(self, randomOffset=None):
+        if randomOffset is None:
+            randomOffset = random.randrange(0, 10000)
+
+        thetaTable = [[scaled_octave_noise_3d(4, 0.5, 3.0,
+                                              loBound=0.0,
+                                              hiBound=1.0,
+                                              x=(thetaIndex * self.thetaSlotWidth),
+                                              y=(rIndex * self.rSlotWidth),
+                                              z=randomOffset)
+                       for rIndex in range(self.rSlots)] for thetaIndex in range(self.thetaSlots)]
+
+        rTable = [[scaled_octave_noise_3d(4, 0.5, 3.0,
+                                          loBound=0.0,
+                                          hiBound=1.0,
+                                          x=(thetaIndex * self.thetaSlotWidth),
+                                          y=(rIndex * self.rSlotWidth),
+                                          z=randomOffset)
+                       for rIndex in range(self.rSlots)] for thetaIndex in range(self.thetaSlots)]
+
+        self.transitionTable = [[None for rIndex in range(self.rSlots)] for thetaIndex in range(self.thetaSlots)]
+        for thetaIndex in range(self.thetaSlots):
+            for rIndex in range(self.rSlots):
+                theta = thetaTable[thetaIndex][rIndex]
+                radius = rTable[thetaIndex][rIndex]
+                self.transitionTable[thetaIndex][rIndex] = self.toXY(theta, radius)
+
+    def randomizeState(self):
+        self.cells = [self.sampleUnitDisk() for i in range(self.numCells)]
+
+    def sampleUnitDisk(self):
+        theta = random.random() * 2 * math.pi
+        radius = random.random()
+        return (math.sqrt(radius) * math.cos(theta), math.sqrt(radius) * math.sin(theta))
+
+    def toXY(self, theta, radius):
+        return radius * math.cos(theta), radius * math.sin(theta)
+
+    def toPolar(self, x, y):
+        return math.atan2(y, x), math.sqrt(x * x + y * y)
+
+    def getCellColors(self):
+        polarStates = [self.toPolar(cell[0], cell[1]) for cell in self.cells]
+        return [self.stateToColor(polarState) for polarState in polarStates]
+
+    def stateToColor(self, state):
+        state = self.toPolar(state[0], state[1])
+        color = colorsys.hsv_to_rgb(state[0], 1, self.sigmoid(state[1]))
+        return [int(component * 255) for component in color]
+
+    def getTransitionTableImage(self):
+        return [[self.stateToColor(self.transitionTable[thetaIndex][rIndex]) for thetaIndex in range(self.thetaSlots)] for rIndex in range(self.rSlots)]
--- display/AutomataDisplay.py
+++ display/AutomataDisplay.py
@@ -1,122 +1,81 @@
 import pygame
 from pygame.locals import *
-from automata.simplexnoise import *
-from automata.MultistateAutomata import MultistateAutomata
-import random
-from automata.utils import *
-from automata.RandomDistribution import RandomDistribution
+from automata.Polar2DAutomata import Polar2DAutomata
+from random import *
+from numpy import *
 import time
 
 class AutomataDisplay:
-    def __init__(self, screenDimensions, initialCells, rule):
+    def __init__(self, screenDimensions, automaton):
+        pygame.init()
         self.screen = pygame.display.set_mode(screenDimensions)
-        self.cells = initialCells
-        self.rule = rule
+        self.automaton = automaton
+        self.screenDimensions = screenDimensions
+        self.resetDisplay()
 
-    def resetToRandom(self):
-        self.cells =
+    def run(self):
+        running = True
+        rownum = 0
+        self.drawTable(300, 300)
+        while running:
+            if rownum == self.screenDimensions[1]:
+                self.doneDrawing = True
+
+            startTime = time.time()
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    running = 0
+
+                if event.type == KEYDOWN:
+                    if event.key == K_RIGHT:
+                        self.resetDisplay()
+                        self.automaton.randomizeRules()
+                        rownum = 0
+                    if event.key == K_UP:
+                        self.resetDisplay()
+                        self.automaton.randomizeState()
+                        rownum = 0
+                    if event.key == K_DOWN:
+                        rownum = 0
+                        self.doneDrawing = False
+
+            if not self.doneDrawing:
+                for cellIndex, cellColor in enumerate(self.automaton.getCellColors()):
+                    self.screen.set_at((cellIndex, rownum), cellColor)
+
+                lineDrawnTime = time.time()
+
+                rownum += 1
+                # rownum = rownum if rownum <=1000 else 0
+                if rownum >= self.screenDimensions[1]:
+                    self.doneDrawing = True
+
+                self.automaton.step()
+
+                stepTime = time.time()
+
+                if rownum % 100 == 0:
+                    pygame.display.flip()
+
+                flipTime = time.time()
+
+                print "\n\nline draw: " + str(lineDrawnTime - startTime) + "\nstep: " + str(stepTime - lineDrawnTime) + "\nflip: " + str(flipTime - stepTime)
+
+        pygame.quit()
+
+    def drawTable(self, x, y):
+        tableSurface = pygame.surfarray.make_surface(array(self.automaton.getTransitionTableImage()))
+        self.screen.blit(tableSurface, (x, y))
+
+    def resetDisplay(self):
+        self.screen.fill((0, 0, 0))
+        self.drawTable(300, 300)
+        self.doneDrawing = False
+        pygame.display.flip()
 
 
-
-
-pygame.init()
-screen = pygame.display.set_mode((1000, 1000))
-
-running = 1
-
-ca = rownum = None
-
-cellsWidth = 500
-
-doneDrawing = False
-
-#how many values each state can take
-stateDepths = [2, 2, 2]
-
-randomDistribution = RandomDistribution(min(stateDepths) - 1)
-
-def randomSubstateProvider():
-    #return random.randint(0, min(stateDepths) - 1)
-    return randomDistribution.getValue()
-
-def reset():
-    global ca
-    global randomDistribution
-    randomDistribution = RandomDistribution(min(stateDepths) - 1)
-    ca = MultistateAutomata(
-        [[random.randint(0,stateDepth-1) for stateDepth in stateDepths] for i in range(cellsWidth)],
-        [indexedRandomMatrix(3, stateDepth, randomSubstateProvider) for stateDepth in stateDepths],
-        stateDepths
-    )
-    global rownum
-    rownum = 0
-    screen.fill((0,0,0))
-    print ca.crossStateRules
-    print randomDistribution.distribution
-
-    global doneDrawing
-    doneDrawing = False
-
-def resetState():
-    global ca
-    global randomDistribution
-    randomDistribution = RandomDistribution(min(stateDepths) - 1)
-    ca = MultistateAutomata(
-        [[random.randint(0, stateDepth - 1) for stateDepth in stateDepths] for i in range(cellsWidth)],
-        ca.ruleTable,
-        stateDepths,
-        ca.crossStateRules
-    )
-    global rownum
-    rownum = 0
-    screen.fill((0, 0, 0))
-
-    global doneDrawing
-    doneDrawing = False
-
-reset()
-
-while running:
-    startTime = time.time()
-    for event in pygame.event.get():
-        print event.type
-        if event.type == QUIT:
-            running = 0
-
-        if event.type == KEYDOWN:
-            if event.key == K_RIGHT:
-                reset()
-            if event.key == K_UP:
-                resetState()
-            if event.key == K_DOWN:
-                rownum = 0
-                doneDrawing = False
-
-    if not doneDrawing:
-        for cellIndex, cellValue in enumerate(ca.getCells()):
-            screen.set_at((cellIndex, rownum), stateToColor(cellValue, max(stateDepths) - 1))
-
-        pygame.draw.line(screen, (0, 0, 0), (500, rownum), (500 + len(ca.getCells()), rownum), 1)
-        for mutatedIndex in ca.applyCrossStateRules():
-            screen.set_at((mutatedIndex + 500, rownum), (255,255,255))
-
-        lineDrawnTime = time.time()
-
-        rownum += 1
-        #rownum = rownum if rownum <=1000 else 0
-        if rownum >= 1000:
-            doneDrawing = True
-
-        ca.step()
-
-        stepTime = time.time()
-
-        if rownum % 100 == 0:
-            pygame.display.update(pygame.Rect(10, 10, 20, 20))
-
-        flipTime = time.time()
-
-    #print "\n\nline draw: " + str(lineDrawnTime - startTime) + "\nstep: " + str(stepTime - lineDrawnTime) + "\nflip: " + str(flipTime - stepTime)
-
-pygame.quit()
-
+automaton = Polar2DAutomata(200)
+automaton.randomizeState()
+automaton.randomizeRules()
+display = AutomataDisplay((600,600), automaton)
+display.run()
